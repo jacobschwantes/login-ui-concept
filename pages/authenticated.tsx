9f1556ac5b8d4e34ef75bb755af848dedb6295da
jsch <jacobschwantes@outlook.com>
2022-06-25 02:21:24 -0500

    create authenticated page

---
+++ pages/authenticated.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return (
+    <div className="bg-black h-screen flex justify-center items-center w-screen">
+      <p className="text-6xl font-bold text-white">Authenticated</p>
+    </div>
+  );
+};
+export default Page;
